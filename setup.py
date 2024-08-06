5d9313df3da099c9e1ff5a3c58f08caf97c6f2ce
DongreShreyas <dshreyas13@gmail.com>
2024-08-06 14:54:20 +0530

    Folder Structure Added

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = 'signLanguages',
+    version= '0.0.0',
+    author= 'Shreyas Dongre',
+    author_email= 'dongreshreyas23@gmail.com',
+    packages= find_packages(),
+    install_requires = []
+
+)
