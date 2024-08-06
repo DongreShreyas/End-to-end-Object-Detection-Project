120aff0b11eff317ddf8b5eb6e8e0e2e5079a3e9
DongreShreyas <dshreyas13@gmail.com>
2024-08-06 16:28:57 +0530

    Folder Structure Added

--- data_collector.py
+++ data_collector.py
@@ -11,17 +11,18 @@ number_of_images = 5
 
 for label in labels:
     img_path = os.path.join(IMAGE_PATH, label)
-    os.makedirs(img_path)
+    os.makedirs(img_path, exist_ok=True)
+
 
     #open camera 
-    cap = cv2.VideoCapture(1)
+    cap = cv2.VideoCapture(0)
     print(f"Collecting images for {label}")
     time.sleep(3)
 
     for imgnum in range(number_of_images):
         ret,frame = cap.read()
         imagename=os.path.join(IMAGE_PATH,label,label+'.'+'{}.jpg'.format(str(uuid.uuid1())))
-        cv2.imwrite('imagename', frame)
+        cv2.imwrite(imagename, frame)
         cv2.imshow('frame', frame)
         time.sleep(2)
 
